1ac0f4ef4bd3e6e778284cdc780b81edd0e6854e
chnguyen <cuong.huu.nguyen@mgm-tp.com>
2023-05-29 14:58:01 +0700

    initial commit

---
+++ src/main/java/com/mgmtp/easyquizy/EasyQuizyBeApplication.java
@@ -0,0 +1,13 @@
+package com.mgmtp.easyquizy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EasyQuizyBeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EasyQuizyBeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/mgmtp/easyquizy/EasyQuizyBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mgmtp.easyquizy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EasyQuizyBeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
