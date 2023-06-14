8b332eb64add9dfdee79ab906b557967a2420321
Minh Van Nhat Dang <80151408+minhkycho18@users.noreply.github.com>
2023-06-14 09:09:48 +0700

    feat: Add Constraint between CategoryEntity and QuestionEntity 14.06.2023

--- server-app/src/main/java/com/mgmtp/easyquizy/model/CategoryEntity.java
+++ server-app/src/main/java/com/mgmtp/easyquizy/model/CategoryEntity.java
@@ -3,6 +3,7 @@ package com.mgmtp.easyquizy.model;
 import lombok.*;
 
 import javax.persistence.*;
+import java.util.List;
 
 @Getter
 @Setter
@@ -20,5 +21,7 @@ public class CategoryEntity {
     @Column(name = "name", length = 20, nullable = false)
     private String name;
 
+    @OneToMany(mappedBy = "categoryEntity", fetch = FetchType.LAZY)
+    private List<QuestionEntity> questionEntities;
 
 }
