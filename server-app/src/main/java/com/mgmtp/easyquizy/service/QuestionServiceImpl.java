b6ec5ba5395196fe3e726dcb45aef8045e4701c9
Diễm Thị Trần <diem.thi.tran@mgm-tp.com>
2023-07-18 04:45:38 +0000

    MGMIDN-829: Generate time limit like Kahoot
    Merge in DA-NANG-INTERNSHIP/easy-quizy-be from feature/MGMIDN-829-generate-time-limit-like-kahoot to develop
    
    Squashed commit of the following:
    
    commit d96fc141ab2e3cc655ae8d874805aeed72e1ba0d
    Author: diemtran2806 <ttdiem2002@gmail.com>
    Date:   Tue Jul 18 11:40:03 2023 +0700
    
        MGMIDN-829: Validate time limit in question validator
    
    commit c7d155be82cb01f65f318b1461dc7ca3d54cc3af
    Author: diemtran2806 <ttdiem2002@gmail.com>
    Date:   Tue Jul 18 10:39:54 2023 +0700
    
        MGMIDN-829: Generate time limit like Kahoot

--- server-app/src/main/java/com/mgmtp/easyquizy/service/QuestionServiceImpl.java
+++ server-app/src/main/java/com/mgmtp/easyquizy/service/QuestionServiceImpl.java
@@ -27,8 +27,7 @@ import org.springframework.transaction.annotation.Transactional;
 import org.springframework.util.StringUtils;
 
 import javax.persistence.criteria.Predicate;
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 
 @Service
 public class QuestionServiceImpl implements QuestionService {
--- server-app/src/main/java/com/mgmtp/easyquizy/validator/QuestionDTOValidator.java
+++ server-app/src/main/java/com/mgmtp/easyquizy/validator/QuestionDTOValidator.java
@@ -5,8 +5,12 @@ import org.springframework.stereotype.Component;
 import org.springframework.validation.Errors;
 import org.springframework.validation.Validator;
 
+import java.util.Arrays;
+import java.util.List;
+
 @Component
 public class QuestionDTOValidator implements Validator {
+    private static final List<Integer> VALID_TIME_LIMIT = Arrays.asList(10, 20, 30, 60, 90, 120, 180, 240);
     @Override
     public boolean supports(Class<?> clazz) {
         return QuestionDTO.class.isAssignableFrom(clazz);
@@ -15,6 +19,10 @@ public class QuestionDTOValidator implements Validator {
     @Override
     public void validate(Object target, Errors errors) {
         QuestionDTO questionDTOs = (QuestionDTO) target;
+
+        if (!VALID_TIME_LIMIT.contains(questionDTOs.getTimeLimit())){
+            errors.rejectValue("timeLimit", "timeLimit.invalid", "Invalid time limit. It must be one of the following values: [10, 20, 30, 60, 90, 120, 180, 240]!");
+        }
 
         if (questionDTOs.getAnswers() == null) {
             errors.rejectValue("answers", "answers.required", "Please provide at least one answer!!!");
