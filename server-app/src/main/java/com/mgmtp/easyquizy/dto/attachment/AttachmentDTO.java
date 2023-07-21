b41132f6f38d764bb4e470728b114a616ad49d2b
Vân Thị Thùy Trần <van.thi.thuy.tran@mgm-tp.com>
2023-07-21 07:58:32 +0000

    MGMIDN-856: Handle image size
    Merge in DA-NANG-INTERNSHIP/easy-quizy-be from feature/MGMIDN-856-be_image-size-must-not-exceed-2mb to develop
    
    Squashed commit of the following:
    
    commit 323a01f5181813ba024d529e61f4455290e9ea7a
    Author: tranthuyvan <tranthuyvan2105@gmail.com>
    Date:   Fri Jul 21 10:35:11 2023 +0700
    
        MGMIDN-856: Handle image size

--- server-app/src/main/java/com/mgmtp/easyquizy/dto/attachment/AttachmentDTO.java
+++ server-app/src/main/java/com/mgmtp/easyquizy/dto/attachment/AttachmentDTO.java
@@ -3,6 +3,7 @@ package com.mgmtp.easyquizy.dto.attachment;
 import lombok.*;
 
 import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
 
 @NoArgsConstructor
 @AllArgsConstructor
@@ -13,6 +14,7 @@ public class AttachmentDTO {
     private Long id;
 
     @NotBlank(message = "This field is required")
+    @Size(max = 1024 * 1024 * 4, message = "Image size must not exceed 4MB")
     private String imageData;
 
     private String sourceCode;
